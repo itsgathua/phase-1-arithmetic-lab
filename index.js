fa29333ade326532f741df805b17bfc78ec8f341
Ian Gathua Mbui <gathuambui@gmail.com>
2024-12-03 20:39:45 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
+console.log(multiply)
+let random = Math.floor(Math.random()* 10);
+let num3 = 19
+let num4 = 5
+let mod = (num3 % num4)
+console.log(mod)
+let max = Math.max (1, 4, 7, 15, 20, 12, 18, 9)
+console.log (max)
